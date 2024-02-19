40c8b9e51ff7ac4ea1e739e6358512a9f11215f1
jiaxi_yang@epam <jiaxi_yang@epam.com>
2024-02-19 11:14:25 +0800

    Add prettier config

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
+import { routes } from './app.routes';
 
 @Component({
   selector: 'app-root',
@@ -9,18 +10,5 @@ import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
   templateUrl: './app.component.html',
 })
 export class AppComponent {
-  links = [
-    {
-      path: 'first',
-      name: 'First Component',
-    },
-    {
-      path: 'second',
-      name: 'Second Component',
-    },
-    {
-      path: 'todo',
-      name: 'Todo Component',
-    },
-  ];
+  links = routes.filter((v) => v.name);
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,24 +1,38 @@
-import { Routes } from '@angular/router';
+import { Routes, Route } from '@angular/router';
 import { FirstComponent } from './first/first.component';
 import { SecondComponent } from './second/second.component';
 import { TodoComponent } from './todo/todo.component';
+import { FormComponent } from './form/form.component';
 
-export const routes: Routes = [
+type TRoute = Route & {
+  name: string;
+};
+
+export const routes: TRoute[] = [
   {
     path: 'first',
     component: FirstComponent,
+    name: 'First Component',
   },
   {
     path: '',
     redirectTo: 'todo',
     pathMatch: 'full',
+    name: 'Todo Component',
   },
   {
     path: 'second',
+    name: 'Second Component',
     component: SecondComponent,
   },
   {
     path: 'todo',
+    name: 'Todo Component',
     component: TodoComponent,
+  },
+  {
+    path: 'form',
+    component: FormComponent,
+    name: 'Form Component',
   },
 ];
