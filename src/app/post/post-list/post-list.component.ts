b91c087ef80b1ea5a63d99a67d7467de280a5e52
jxyang <jxyang.dev@gmail.com>
2024-02-27 14:51:52 +0800

    Replaced ngfor with @for

--- src/app/post/post-list/post-list.component.ts
+++ src/app/post/post-list/post-list.component.ts
@@ -2,12 +2,11 @@ import { Component, OnInit } from "@angular/core";
 import { TPost } from "../post-service/post.interface";
 import { CommonModule } from "@angular/common";
 import { ActivatedRoute, RouterModule } from "@angular/router";
-import { LoadingErrorIndicatorComponent } from "../../common/loading-error-indicator/loading-error-indicator.component";
 
 @Component({
   selector: "app-post-list",
   standalone: true,
-  imports: [CommonModule, RouterModule, LoadingErrorIndicatorComponent],
+  imports: [CommonModule, RouterModule],
   templateUrl: "./post-list.component.html",
 })
 export class PostListComponent implements OnInit {
