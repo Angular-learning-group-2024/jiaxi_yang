209e7e4ef10500ae6e4c904f0244f2f3e64c03c0
Jiaxi Yang <yangjx1943@gmail.com>
2024-03-14 11:02:08 +0800

    Update e2e test

---
+++ e2e/app.spec.ts
@@ -0,0 +1,9 @@
+import { expect, test } from "@playwright/test";
+
+test.beforeEach(async ({ page }) => {
+  await page.goto("/");
+});
+
+test("has title", async ({ page }) => {
+  await expect(page).toHaveTitle("Angular Learning");
+});
